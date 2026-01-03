3c14d723fc150971ec045fac716f9420bf41d3cd
nghiax <nghialk11@gmail.com>
2026-01-03 12:00:54 +0700

    Refactor TripSearchRequest and SearchLog to use Instant for date fields; update TripService to handle timezone and apply 30-minute departure rule

--- services/booking-service/src/main/java/com/booking/bookingService/model/SearchLog.java
+++ services/booking-service/src/main/java/com/booking/bookingService/model/SearchLog.java
@@ -3,13 +3,18 @@ package com.booking.bookingService.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "search_log")
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SearchLog {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -17,7 +22,7 @@ public class SearchLog {
 
     private String origin;
     private String destination;
-    
-    private LocalDate travelDate; // The date user wanted to go
+
+    private Instant travelDate; // The date user wanted to go
     private LocalDateTime searchedAt; // The time they clicked "Search"
 }
